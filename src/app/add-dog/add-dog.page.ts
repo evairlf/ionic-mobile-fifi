3da49f506a5bf58de4f37cbefe2797b6faaad8d7
Evair Leite Feldmann <evairleitefeldmann@gmail.com>
2021-09-23 21:08:16 -0300

    Adcionado galeria

--- src/app/add-dog/add-dog.page.ts
+++ src/app/add-dog/add-dog.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, DestinationType, PictureSourceType } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-add-dog',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDogPage implements OnInit {
 
-  constructor() { }
-
+  constructor(private camera: Camera) { }
+  imgURL;
   ngOnInit() {
   }
 
+  getCamera(){
+    this.camera.getPicture({
+      sourceType:  this.camera.PictureSourceType.CAMERA,
+      destinationType: this.camera.DestinationType.FILE_URI
+    }).then((res)=>{
+      this.imgURL = res;
+    }).catch(e=> {
+      console.log(e);
+    })
+  }
+
+  getGallery(){
+    this.camera.getPicture({
+      sourceType:  this.camera.PictureSourceType.PHOTOLIBRARY,
+      destinationType: this.camera.DestinationType.DATA_URL
+    }).then((res)=>{
+      this.imgURL = 'data:image/jpeg;base64,' + res;
+    }).catch(e=> {
+      console.log(e);
+    })
+  }
 }
